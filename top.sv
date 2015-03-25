961a370de06c15b651aa620a1890101bb1b8a2dd
Guillaume Legrain <legrainguillaume@gmail.com>
2015-03-25 16:18:48 -0700

    Created top module and testbench

---
+++ top.sv
@@ -0,0 +1,34 @@
+// Game Controller top module for tic-tac-toe project
+// Written by Katherine Yang and Guillaume Legrain
+// Written in: March 25, 2015
+// player1:11, player2:10, tie:01, noWin:00
+// cellState: empty:00, player1:11, player2:10
+/////////////////////////////////////////////////////
+module top(input  logic        ph1, ph2, reset,
+           input  logic        isPlayer1Start,
+           input  logic        playerWrite,
+           input  logic  [3:0] playerInput,
+           output logic [17:0] gBoard,
+           output logic  [2:0] outputState,
+           output logic        gameIsDone,
+           output logic  [1:0] winner);
+  
+  logic  [3:0] addr;
+  logic  [1:0] cellState;
+  
+  gameController dut(.ph1, .ph2, .reset,
+                     .isPlayer1Start,
+                     .playerWrite,
+                     .playerInput,
+                     .gBoard,
+                     .gameIsDone,
+                     .winner,
+                     .addr,
+                     .outputState,
+                     .cellState);
+
+  memArray dut2(.ph1, .ph2, .reset, .addr, .cellState, .gBoard);
+
+  winLogic winLogic1(.gBoard, .gameIsDone, .winner);
+
+ endmodule
---
+++ top_tb.sv
@@ -0,0 +1,74 @@
+module top_tb();
+  logic        ph1, ph2, reset;
+  logic        isPlayer1Start;
+  logic        playerWrite;
+  logic  [3:0] playerInput;
+  logic [17:0] gBoard;
+  logic  [2:0] outputState;
+  logic        gameIsDone;
+  logic  [1:0] winner;
+
+  logic [17:0] gBoardExpected;
+  logic [31:0] vectornum, errors;
+  logic [22:0] testvectors[10000:0]; 
+
+  
+  // instantiate device under test
+  top dut(.ph1, .ph2, .reset,
+          .isPlayer1Start,
+          .playerWrite,
+          .playerInput,
+          .gBoard,
+          .outputState,
+          .gameIsDone,
+          .winner);
+
+  // generate clock to sequence tests
+  always
+    begin
+     ph1 = 0; ph2 = 0; #1; 
+     ph1 = 1; # 4; 
+     ph1 = 0; # 1; 
+     ph2 = 1; # 4;
+    end
+
+   // tell the simulator to store the waveform into a file for inspection
+  // and start dumping all signal to the .vcd file
+  initial
+    begin
+      $dumpfile("top.vcd");
+      $dumpvars;
+    end
+
+  // at start of test, load test vectors
+  initial
+    begin
+      $readmemb("top.tv", testvectors);
+      vectornum=0; errors=0;
+      reset=1; #17; reset=0;
+    end
+
+  // apply test vectors on rising edge of clk
+  always @(posedge ph2)
+    begin
+      #1; {playerInput, gBoardExpected} = testvectors[vectornum];
+    end
+
+  // check results on falling edge of clk
+  always @(negedge ph2)
+    if(~reset) begin // skip during reset
+      if (gBoard !== gBoardExpected) begin // check result
+        $display("Error: vectornum=%d", vectornum);
+        $display("outputs=%b (%b expected)", gBoard, gBoardExpected);
+        errors = errors + 1;
+      end
+      vectornum = vectornum + 1;
+      if(testvectors[vectornum] === 23'bx) begin
+        $display("%d tests completed with %d errors", vectornum, errors);
+        $finish;
+      end
+    end
+    
+    
+
+endmodule
