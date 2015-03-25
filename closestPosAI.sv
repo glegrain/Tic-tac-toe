29f28b5f044cc7bcde55b426d0e9c482fbc6f213
Katherine Yang <katherine_yang@hmc.edu>
2015-03-25 01:21:08 -0700

    wrote up AI, needs testing

--- closestPosAI.sv
+++ closestPosAI.sv
@@ -7,7 +7,7 @@
 /////////////////////////////////////////////////////
 
 // states
-typedef enum logic [2:0] {WAIT, CHECKROW0, CHECKROW1, CHECKROW2} statetype;
+typedef enum logic [2:0] {WAIT, CHECKROW0, CHECKROW1, CHECKROW2, END} statetype;
 
 // This module checks if the row is full
 module isRowFull(input logic [5:0] gBoardRow,
@@ -54,75 +54,74 @@ module closestEmptyPos(input logic [5:0] gBoardRow,
       9'b011???0?1: addr<=4'b0111;
       9'b011?0?1?1: addr<=4'b1000;
       
-      default: addr<=4'b11111; //bad address
+      default: addr<=4'b1111; //bad address
     endcase 
   end 
 endmodule
 
 module closestPosAI(input  logic          ph1, ph2, reset,
-                      input  logic [2:0]  cellState,
+                      input  logic  [1:0] cellState, //our AI is when the cellState is 2'b10
                       input  logic [17:0] gBoard,
-                      output logic  [3:0] addr);
-  statetype state;
+                      output logic  [3:0] addr,
+                      output logic        writeToBoard);
+  statetype [2:0] state;
+  logic [5:0] gBoardRow;
+  logic rowIsFull, isTurn;
+  // assign the gBoard row according to states
+  extractRow findRow(gBoard, state, gBoardRow);
+  // find if current row is full
+  isRowFull rowFull(gBoardRow, rowIsFull);
+  // find the address of the top leftmost empty square
+  closestEmptyPos assignAddr(gBoardRow, state, addr);
 
+  assign isTurn = cellState[1] & (~cellState[0]);
+
+  outputlogic outputlog(state, isFull, writeToBoard); 
   // control FSM
   statelogic  statelog(.ph1, .ph2, .reset,
-                       .isPlayer1Start, .gameIsDone, .playerWrite, .state);
-  outputlogic outputlog(.state, .playerWrite, .playerInput, .addr, .cellState);
+                       .isTurn, .rowIsFull, .state);
+  //outputlogic outputlog(.state, .playerWrite, .playerInput, .addr, .cellState);
 
 endmodule
 
 
 module statelogic(input  logic     ph1, ph2, reset,
-                  input  logic     isPlayer1Start,
-                  input  logic     gameIsDone,
-                  input  logic     playerWrite,
-                  output statetype state);
+                  input  logic     isTurn, rowIsFull
+                  output statetype[2:0] state);
 
-  statetype nextstate;
-  logic [1:0] ns, state_logic;
+  statetype [2:0]nextstate;
+  logic [2:0] ns, state_logic;
 
   // resetable state register with initial value of START
-  mux2 #(2) resetmux(nextstate, START, reset, ns);
-  flop #(2) stateregister(ph1, ph2, ns, state_logic);
+  mux2 #(3) resetmux(nextstate, WAIT, reset, ns);
+  flop #(3) stateregister(ph1, ph2, ns, state_logic);
   assign state = statetype'(state_logic);
 
   // next state logic
   always_comb
     begin
       case (state)
-        START:   nextstate = (isPlayer1Start) ? PLAYER1 : PLAYER2;
-        PLAYER1: if (gameIsDone) nextstate = END;
-                 else if (playerWrite) nextstate = PLAYER2;
-                 else nextstate = PLAYER1;
-        PLAYER2: if (gameIsDone) nextstate = END;
-                 else if (playerWrite) nextstate = PLAYER1;
-                 else nextstate = PLAYER1;
-        END:     nextstate = END;
-        default: nextstate = START;
+        WAIT:      nextstate = (isTurn) ? CHECKROW0 : WAIT;
+        CHECKROW0: nextstate = (isFull) ? CHECKROW1 : WAIT;
+        CHECKROW1: nextstate = (isFull) ? CHECKROW2 : WAIT;
+        CHECKROW2: nextstate = (isFull) ? END : WAIT;
+        END:       nextstate = END;
+        default: nextstate = WAIT;
       endcase
     end
 endmodule
 
-module outputlogic(input  statetype   state,
-                   input  logic       playerWrite,
-                   input  logic [3:0] playerInput,
-                   output logic [3:0] addr,
-                   output logic [1:0] cellState);
+module outputlogic(input  statetype [2:0] state,
+                   input logic  isFull,
+                   output logic writeToBoard);
 
+  logic [3:0]combState = {isFull, state};
   always_comb
     begin
-      // NOTE: Assuming playerWrite is enable bit active on one clock cycle.
-      //       A sperate module can be used to detect the playerWrite rising edge.
-
-      // always send cellState to memory but will write only
-      // on valid addr (addr of 9'b0 won't write anything)
-      addr = (playerWrite) playerInput : 9'b000000000;
-      if (state == PLAYER1)
-        cellState = 2'b11;
-      else if (state == PLAYER2)
-        cellState = 2'b10;
-      else
-        cellState = 2'b00;
-    end
+      case (state)
+        4'b0001: writeToBoard <= 1'b1;
+        4'b0010: writeToBoard <= 1'b1;
+        4'b0011: writeToBoard <= 1'b1;
+        default: writeToBoard <= 1'b0;
+      endcase
 endmodule
