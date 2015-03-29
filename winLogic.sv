47d6bde1fd0c492093f96f81f0f30947523ffbc7
Guillaume Legrain <legrainguillaume@gmail.com>
2015-03-28 21:39:51 -0700

    updated winLogic to use the isSame module

--- winLogic.sv
+++ winLogic.sv
@@ -8,39 +8,45 @@
 module winLogic(input  logic[17:0] gBoard,
                 output logic gameIsDone,
                 output logic[1:0] winner); // player1: 11, player2: 10, tie: 01, noWin: 00
-logic col0IsFull, col1IsFull, col2IsFull;
-logic col0IsSame, col1IsSame, col2IsSame;
-logic row0IsFull, row1IsFull, row2IsFull;
-logic row0IsSame, row1IsSame, row2IsSame;
-logic diag0IsFull, diag1IsFull;
-logic diag0IsSame, diag1IsSame;
-logic gameIsTie, thereIsAWin;
+  logic col0IsFull, col1IsFull, col2IsFull;
+  logic col0IsSame, col1IsSame, col2IsSame;
+  logic row0IsFull, row1IsFull, row2IsFull;
+  logic row0IsSame, row1IsSame, row2IsSame;
+  logic diag0IsFull, diag1IsFull;
+  logic diag0IsSame, diag1IsSame;
+  logic gameIsTie, thereIsAWin;
 
-// each cell is represented by two bits in gBoard
-// empty:   00
-// player1: 11
-// player2: 10
+  // each cell is represented by two bits in gBoard
+  // The MSB tells if the cell has been played or not
+  // The LSB tells
+  // empty:   00
+  // player1: 11
+  // player2: 10
+
+  // Check the LSB of each cell to see if they are the same
+  isSame col0IsSameMod(gBoard[1], gBoard[7], gBoard[13], col0IsSame);
+  isSame col1IsSameMod(gBoard[3], gBoard[9], gBoard[15], col1IsSame);
+  isSame col2IsSameMod(gBoard[5], gBoard[11], gBoard[17], col2IsSame);
+ 
+  isSame row0IsSameMod(gBoard[1], gBoard[3], gBoard[5], row0IsSame);
+  isSame row1IsSameMod(gBoard[7], gBoard[9], gBoard[11], row1IsSame);
+  isSame row2IsSameMod(gBoard[13], gBoard[15], gBoard[17], row2IsSame);
+
+  isSame diag0IsSameMod(gBoard[1], gBoard[9], gBoard[17], diag0IsSame);
+  isSame diag1IsSameMod(gBoard[5], gBoard[9], gBoard[13], diag1IsSame);
+
   always_comb begin
-    // First check that the column is filled 
-    // and then check that they are all the same
+    // Check the MSB of the cell to see if it has been written
     col0IsFull = (gBoard[0] & gBoard[6] & gBoard[12]);
-    col0IsSame = ~((gBoard[1] ^ gBoard[7]) | (gBoard[13] ^ gBoard[7]));
     col1IsFull = gBoard[2] & gBoard[8] & gBoard[14];
-    col1IsSame = ~((gBoard[3] ^ gBoard[9]) | (gBoard[15] ^ gBoard[9]));
     col2IsFull = gBoard[4] & gBoard[10] & gBoard[16];
-    col2IsSame = ~((gBoard[5] ^ gBoard[11]) | (gBoard[17] ^ gBoard[11]));
 
     row0IsFull = gBoard[0] & gBoard[2] & gBoard[4];
-    row0IsSame = ~((gBoard[1] ^ gBoard[3]) | (gBoard[5] ^ gBoard[3]));
     row1IsFull = gBoard[6] & gBoard[8] & gBoard[10];
-    row1IsSame =  ~((gBoard[7] ^ gBoard[9]) | (gBoard[11] ^ gBoard[9]));
     row2IsFull = gBoard[12] & gBoard[14] & gBoard[16];
-    row2IsSame =  ~((gBoard[13] ^ gBoard[15]) | (gBoard[17] ^ gBoard[15]));
 
     diag0IsFull = gBoard[0] & gBoard[8] & gBoard[16];
-    diag0IsSame = ~((gBoard[1] ^ gBoard[9]) | (gBoard[17] ^ gBoard[9]));
     diag1IsFull = gBoard[4] & gBoard[8] & gBoard[12];
-    diag1IsSame = ~((gBoard[5] ^ gBoard[9]) | (gBoard[13] ^ gBoard[9]));
 
     // If all columns are filled, there is no more free space.
     gameIsTie = col0IsFull & col1IsFull & col2IsFull;
